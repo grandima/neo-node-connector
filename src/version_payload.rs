856826abf89d5911080ac8738b330383feeff84d
Dmytro Medynskyi <grandima@gmail.com>
2023-10-13 14:52:37 +0200

    fix version_payload.rs

--- src/version_payload.rs
+++ src/version_payload.rs
@@ -1,9 +1,9 @@
-use bincode::*;
-use bincode::enc::Encoder;
-use bincode::error::EncodeError;
-use crate::Capability::{Capability, FULL_NODE, TCP_SERVER, WS_SERVER};
+use crate::capability::{Capability, FULL_NODE, TCP_SERVER, WS_SERVER};
 use crate::neoi64::NEOi64;
 use crate::user_agent::UserAgent;
+use bincode::enc::Encoder;
+use bincode::error::EncodeError;
+use bincode::*;
 use std::time::*;
 
 #[derive(Debug)]
@@ -18,18 +18,16 @@ pub struct VersionPayload {
 
 impl Default for VersionPayload {
     fn default() -> Self {
-        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default();
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap_or_default();
         Self {
             network: 5943216,
             version: 0,
             timestamp: now.as_secs() as u32,
             nonce: 2100635172,
             user_agent: "/Neo:3.6.0/".into(),
-            capabilities: vec![
-                FULL_NODE,
-                TCP_SERVER,
-                WS_SERVER,
-            ],
+            capabilities: vec![FULL_NODE, TCP_SERVER, WS_SERVER],
         }
     }
 }
@@ -43,7 +41,9 @@ impl Encode for VersionPayload {
         Encode::encode(&self.user_agent, encoder)?;
         let len = (self.capabilities.len() as i64);
         Encode::encode(&NEOi64::from(len), encoder)?;
-        self.capabilities.iter().for_each(|item| { _ = Encode::encode(item, encoder); });
+        self.capabilities.iter().for_each(|item| {
+            _ = Encode::encode(item, encoder);
+        });
         Ok(())
     }
 }
